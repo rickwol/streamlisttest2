02c86739293428289223544901940658c9feeb6a
rickwol <r.wolbertus@hva.nl>
2023-03-08 12:42:11 +0100

    Add files via upload

---
+++ app7.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#pip install streamlit
+
+
+# In[6]:
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+# In[ ]:
+
+
+HairEye = pd.read_csv("HairEyeColor.csv")
+
+
+# In[ ]:
+
+
+st.title("Hair Eye Colour Database")
+
+
+# In[ ]:
+
+
+InputHair = st.sidebar.selectbox("Select Hair Colour", ("Brown", "Black", "Blond", "Red"))
+
+
+# In[ ]:
+
+
+HairEyeSelect = HairEye[HairEye["Hair"] == InputHair]
+
+
+# In[ ]:
+
+
+st.dataframe(HairEyeSelect)
+
+
+# In[1]:
+
+
+import kaggle
+%env KAGGLE_USERNAME="rickwolbertus"
+%env KAGGLE_KEY="e5855257a16718159038159ac864b8f7"
+
+# In[2]:
+
+
+get_ipython().system('kaggle datasets download -d sunayanagawde/countrywise-covid-cases')
+
+
+# In[4]:
+
+
+from zipfile import ZipFile
+
+with ZipFile('countrywise-covid-cases.zip', 'r') as f:
+    f.extractall()
+
+
+# In[7]:
+
+
+covid = pd.read_csv("Country-wise-COVID-cases.csv")
+
+
+# In[ ]:
+
+
+st.dataframe(covid)
+
