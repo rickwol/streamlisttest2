e9b543081615f6fc249ca05cefa32428bf6c1fcf
rickwol <r.wolbertus@hva.nl>
2023-03-08 12:56:46 +0100

    Create kaggle.py

---
+++ kaggle.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from kaggle.api.kaggle_api_extended import KaggleApi
+import streamlit as st
+
+%env KAGGLE_USERNAME="rickwolbertus"
+%env KAGGLE_KEY="e5855257a16718159038159ac864b8f7"
+
+dataset_download_files("adiamaan/modi-speeches", path="./data", unzip=True)
+df = pd.read_csv("./data/modi_speeches.csv")
+st.dataframe(df)
