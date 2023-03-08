91d983f03b19d4fc7fc7372751fb7ee980e558b5
rickwol <r.wolbertus@hva.nl>
2023-03-08 12:45:53 +0100

    Add files via upload

---
+++ app8.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#pip install streamlit
+
+
+# In[8]:
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+# In[9]:
+
+
+HairEye = pd.read_csv("HairEyeColor.csv")
+
+
+# In[10]:
+
+
+st.title("Hair Eye Colour Database")
+
+
+# In[ ]:
+
+
+InputHair = st.sidebar.selectbox("Select Hair Colour", ("Brown", "Black", "Blond", "Red"))
+
+
+# In[ ]:
+
+
+HairEyeselect = HairEye[HairEye["Hair"] == InputHair]
+
+
+# In[ ]:
+
+
+st.dataframe(HairEyeSelect)
+
+
+# In[15]:
+
+
+import kaggle
+get_ipython().run_line_magic('env', 'KAGGLE_USERNAME="rickwolbertus"')
+get_ipython().run_line_magic('env', 'KAGGLE_KEY="e5855257a16718159038159ac864b8f7"')
+
+
+# In[13]:
+
+
+from importlib.metadata import version
+version('kaggle')
+
+
+# In[2]:
+
+
+get_ipython().system('kaggle datasets download -d sunayanagawde/countrywise-covid-cases')
+
+
+# In[4]:
+
+
+from zipfile import ZipFile
+
+with ZipFile('countrywise-covid-cases.zip', 'r') as f:
+    f.extractall()
+
+
+# In[7]:
+
+
+covid = pd.read_csv("Country-wise-COVID-cases.csv")
+
+
+# In[ ]:
+
+
+st.dataframe(covid)
+
