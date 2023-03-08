57ee64aac185800eea1a09d617d7aa60167768c0
rickwol <r.wolbertus@hva.nl>
2023-03-08 13:03:40 +0100

    Update kaggle.py

--- kaggle.py
+++ kaggle.py
@@ -2,9 +2,10 @@ import pandas as pd
 from kaggle.api.kaggle_api_extended import KaggleApi
 import streamlit as st
 
-env:
-          KAGGLE_USERNAME: ${{"rickwolbertus"}}
-          KAGGLE_KEY: ${{"e5855257a16718159038159ac864b8f7"}}
+import os
+
+os.environ['KAGGLE_USERNAME'] = "'rickwolbertus"
+os.environ['KAGGLE_KEY'] = "e5855257a16718159038159ac864b8f7"
 
 dataset_download_files("adiamaan/modi-speeches", path="./data", unzip=True)
 df = pd.read_csv("./data/modi_speeches.csv")
